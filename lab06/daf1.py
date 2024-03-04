dba6c2c0b9c6005d7427e79a9ac62930b30ef8cc
suttegi <esenerlan06@gmail.com>
2024-03-04 17:32:27 +0000

    addLaba06

---
+++ lab06/daf1.py
@@ -0,0 +1,4 @@
+import os
+path = os.getcwd()
+print(path)
+# print(f"Files: {[f for f in os.listdir(path) if os.path.isfile(f)]}, directories: {[d for d in os.listdir(path) if os.path.isdir(d)]} all elements are: {os.listdir(path)}")
